e482733b9c631551d1c0a8c7ffb304a60140a966
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-01-20 12:47:23 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class MultiplicationTable{
+    public static void printMultiplicationTable(int number){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the number : ");
+    int n = sc.nextInt();
+    for(int i = 1; i <= 10; i++){
+        System.out.println(n + " X " + i + " = " + n*i);
+    }
+    }
+    public static void main(String args[]){
+      printMultiplicationTable(5);
+    }
+}
